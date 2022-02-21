4a1a5bbd7af0c9c5b061383c34df8bd7724b9192
monica-tuttle <78395529+monica-tuttle@users.noreply.github.com>
2022-02-21 18:40:58 -0500

    Delete ice_cream_flavors.py

--- ice_cream_flavors.py
+++
@@ -1,9 +0,0 @@
-favorite_ice_cream = { 1 : "cherry chocolate",
-                       2 : "vanilla bean",
-                       3 : "cookie dough",
-                       4 : "dulce de leche",
-                       5 : "mango",
-                       6 : "mint chocolate chip"}
-
-for key in favorite_ice_cream:
-    print(key, ": ", favorite_ice_cream[key], sep="")
